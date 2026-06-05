c1681fde40decac6d13f6403082ab12a85be8a5e
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-05 09:45:51 +0300

    the main app of practice have been removed and then new project main have been set

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from 'react'
 
-
-const Card = ({title}) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  
+export default function App() {
   return (
-    <div className='card '>
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(true)}>
-        Like
-      </button>
-    </div>
+    <div>App</div>
   )
 }
-
-const App = () => {
-  return (
-    <div className='car-container'>
-      <Card title="star war" rating={5} isCool={true} actors={[{name: "Actors"}]}/>
-      <Card title="avatar"/>
-      <Card title="in the land"/>
-    </div>
-  )
-}
-
-export default App
