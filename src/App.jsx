fe18e6b1995034cec51203f0625fed909cc7215b
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-05 15:20:44 +0300

    the tailwind in the main app have been adde

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <div className="pattern"/>
+
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+    </main>
   )
 }
+
+export default App
